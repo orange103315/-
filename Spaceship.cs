9a49922b70574c22e5f459fb3b61af1746113f36
平兮　信吾 <1816230@s.asojuku.ac.jp>
2020-05-21 16:23:58 +0900

    課題

--- Spaceship.cs
+++ Spaceship.cs
@@ -1,42 +1,9 @@
-<<<<<<< HEAD
+
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Spaceship : MonoBehaviour
 {
-	// 移動スピード
-	public float speed;
-	
-	// 弾を撃つ間隔
-	public float shotDelay;
-	
-	// 弾のPrefab
-	public GameObject bullet;
-	
-	// 弾を撃つかどうか
-	public bool canShot;
-	
-	// 爆発のPrefab
-	public GameObject explosion;
-	
-	// 爆発の作成
-	public void Explosion ()
-	{
-		Instantiate (explosion, transform.position, transform.rotation);
-	}
-	
-	// 弾の作成
-	public void Shot (Transform origin)
-	{
-		Instantiate (bullet, origin.position, origin.rotation);
-	}
-=======
-﻿using UnityEngine;
-
-[RequireComponent(typeof(Rigidbody2D))]
-public class Spaceship : MonoBehaviour
-{
-
     // 移動スピード
     public float speed;
 
@@ -63,5 +30,5 @@ public class Spaceship : MonoBehaviour
     {
         Instantiate(bullet, origin.position, origin.rotation);
     }
->>>>>>> 8486366bc9986d77a2db27f72467a57ca89b0357
+
 }
